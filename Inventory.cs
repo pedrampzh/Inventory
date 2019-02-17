258edf0897a09ac24d7f213c4d00fffd142af879
pedrampzh <47600230+pedrampzh@users.noreply.github.com>
2019-02-17 02:36:29 -0800

    Add files via upload

---
+++ Inventory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invertori
+{
+    class Inventory:BaseClass
+    {
+        public override Guid id
+        {
+            get { return new Guid(); }
+        }
+        public string nameinventori{set; get; }
+        public string adress{set; get;}
+        public string datein{set; get;}
+        public string dateoute{set; get;}
+        List<DocumentProduct> document ;
+    }
+}
